27317034012449c944f568d190eb0e744acc3053
nattzchukwumela <chukwumelapraise@gmail.com>
2024-11-20 17:41:44 +0100

    created a layout for the pdp page, so that product display can have a nice look

--- src/pages/PDP/PDP.jsx
+++ src/pages/PDP/PDP.jsx
@@ -1,19 +1,37 @@
 import { useParams } from "react-router-dom"
 import { CLOTHS_DATA } from "../../db/CLOTHS_DATA";
+import { Navbar } from '../../component/Navbar/Navbar';
+import { Footer } from "../../component/Footer/Footer";
+import './pdp.css'
 
 export function PDP() {
 
     const { id } = useParams();
-    console.log(id);
 
-    const findProductById = CLOTHS_DATA.men.find(item => item.id === id);
+    const product = CLOTHS_DATA.men.find(item => item.id === id);
 
     return (<>
-        <h2>PDP</h2>
+        <Navbar />
         <div className="pdp-container">
-            <div className="pdp-image">
-                <img src={findProductById.image} alt="" />
+            <div className="grid">
+                <section className="pdp-image">
+                    <img src={product.image} alt="" />
+                </section>
+                <section className="item-details">
+                    <div className="one">
+                        <h1>{product.name}</h1>
+                        <p>{product.description}</p>
+                    </div>
+                    <div className="two">
+                        <span id="item_price">{product.price}</span>
+                        <div className="btn-con">
+                            <button className="btn buy-now">Buy Now</button>
+                            <button className="btn add-to-cart">Add to Cart</button>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
+        <Footer />
     </>)
 }
